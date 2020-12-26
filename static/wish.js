9828a03c4a400f61f6f47d12bdc4e8f32434506b
Emrys <Emrysqeens@gmail.com>
2020-12-26 07:54:47 +0100

    Reduced milli seconds to print wishes

--- static/wish.js
+++ static/wish.js
@@ -15,5 +15,5 @@ document.addEventListener('DOMContentLoaded', e=>{
     let writer = setInterval(()=>{
         p.innerText = wisher.substr(0,counter++)
         if ( p.innerText === wisher) clearInterval(writer)
-    }, .0000005);
+    }, 0);
 })
